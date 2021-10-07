41a12abe7e1514c03c8c7176e4aac8b753a89add
etherblood <philipp@jobach.de>
2021-10-08 00:42:21 +0200

    preview fix

--- releng/deploy.sh
+++ releng/deploy.sh
@@ -7,7 +7,7 @@ rm -r ${CLIENT}etherworld
 unzip ${GIT}jlink/target/Etherworld-*.zip -d ${CLIENT}etherworld/
 rsync -rc --delete ${GIT}glue/target/assets ${CLIENT}
 rsync -rc --delete ${GIT}glue/target/lib ${CLIENT}
-mv ${GIT}releng/background.png /var/www/destrostudios/launcher/images/
-mv ${GIT}releng/tile.png /var/www/destrostudios/launcher/images/
+mv ${GIT}releng/background.png /var/www/destrostudios/launcher/images/background_8.png
+mv ${GIT}releng/tile.png /var/www/destrostudios/launcher/images/tile_8.png
 curl https://destrostudios.com:8080/apps/8/updateFiles
 echo done
