6b2b495d1eb29e5c97fd79d2343915352dee84ac
etherblood <philipp@jobach.de>
2021-10-07 21:24:43 +0200

    deploy fix 3

--- releng/deploy.sh
+++ releng/deploy.sh
@@ -6,6 +6,6 @@ CLIENT=$2
 rm -r ${CLIENT}etherworld
 unzip ${GIT}jlink/target/Etherworld-*.zip -d ${CLIENT}etherworld/
 rsync -rc --delete ${GIT}glue/target/assets ${CLIENT}
-rsync -rc --delete ${GIT}glue/target/libs ${CLIENT}
+rsync -rc --delete ${GIT}glue/target/lib ${CLIENT}
 curl https://destrostudios.com:8080/apps/8/updateFiles
 echo done
