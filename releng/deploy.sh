d18fb1020e3a69ae8050b5418d8458abc94c3bb1
etherblood <philipp@jobach.de>
2021-10-08 14:52:39 +0200

    minor file path updates

--- releng/deploy.sh
+++ releng/deploy.sh
@@ -3,11 +3,11 @@
 GIT=$1
 CLIENT=$2
 
-rm -r ${CLIENT}etherworld
-unzip ${GIT}jlink/target/Etherworld-*.zip -d ${CLIENT}etherworld/
+rm -r ${CLIENT}java
+unzip ${GIT}jlink/target/jlink-*.zip -d ${CLIENT}java/
 rsync -rc --delete ${GIT}glue/target/assets ${CLIENT}
 rsync -rc --delete ${GIT}glue/target/lib ${CLIENT}
-mv ${GIT}releng/background.png /var/www/destrostudios/launcher/images/background_8.png
-mv ${GIT}releng/tile.png /var/www/destrostudios/launcher/images/tile_8.png
+cp ${GIT}releng/background.png /var/www/destrostudios/launcher/images/background_8.png
+cp ${GIT}releng/tile.png /var/www/destrostudios/launcher/images/tile_8.png
 curl https://destrostudios.com:8080/apps/8/updateFiles
 echo done
