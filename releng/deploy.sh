0b440627dd3320b2a0586e7c62da2ef566c6fd2a
Etherblood <etherblood@users.noreply.github.com>
2021-10-10 18:39:54 +0200

    Update deploy.sh

--- releng/deploy.sh
+++ releng/deploy.sh
@@ -3,11 +3,11 @@
 GIT=$1
 CLIENT=$2
 
-rm -r ${CLIENT}java
-unzip ${GIT}jlink/target/jlink-*.zip -d ${CLIENT}java/
-rsync -rc --delete ${GIT}glue/target/assets ${CLIENT}
-rsync -rc --delete ${GIT}glue/target/lib ${CLIENT}
-cp ${GIT}releng/background.png /var/www/destrostudios/launcher/images/background_8.png
-cp ${GIT}releng/tile.png /var/www/destrostudios/launcher/images/tile_8.png
+rm -r ${CLIENT}/java
+unzip ${GIT}/jlink/target/jlink-*.zip -d ${CLIENT}/java/
+rsync -rc --delete ${GIT}/glue/target/assets ${CLIENT}/
+rsync -rc --delete ${GIT}/glue/target/lib ${CLIENT}/
+cp ${GIT}/releng/background.png /var/www/destrostudios/launcher/images/background_8.png
+cp ${GIT}/releng/tile.png /var/www/destrostudios/launcher/images/tile_8.png
 curl https://destrostudios.com:8080/apps/8/updateFiles
 echo done
